df9c98639437691963327ad26fe665e4caffb530
nafnu <21520@student.dorset-college.ie>
2021-04-11 10:27:00 +0000

    commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: "images/bw1.jpg",
+    image2: "images/bw2.jpg",
+    displacementImage: "images/diss.png"
+});
